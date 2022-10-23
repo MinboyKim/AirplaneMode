724b7f6ea346191abd065fb1d89df9427a40ebfb
LownC <jwk0020@naver.com>
2022-10-24 03:39:46 +0900

    ws1
    T_T

--- integrity_check.h
+++ integrity_check.h
@@ -14,7 +14,6 @@ void path_check();
 void userList_check();
 void available_check();
 void rule_check();
-vector<string> get_files_indirectory(const string &_path, const string &_filter);
 bool user_integrity_check(string str);
 bool airplane_integrity_check(string str);
 
