0fd8a205bcda9afe2c71eedf6ceeef1044b9572f
jaesang <jaesang00@konkuk.ac.kr>
2022-10-24 03:29:05 +0900

    define

--- integrity_check.h
+++ integrity_check.h
@@ -1,16 +1,21 @@
-#include <iostream>
-#include <io.h>
-#include <fstream>
-#include <vector>
+#ifndef INTEGRITY_CHECK_H
+#define INTEGRITY_CHECK_H
+
+#include "string_function.h"
 #include <Windows.h>
 #include <filesystem>
-#include "string_function.h"
+#include <fstream>
+#include <io.h>
+#include <iostream>
+#include <vector>
 
 void integrity_check();
 void path_check();
 void userList_check();
 void available_check();
 void rule_check();
-vector<string> get_files_indirectory(const string& _path, const string& _filter);
+vector<string> get_files_indirectory(const string &_path, const string &_filter);
 bool user_integrity_check(string str);
 bool airplane_integrity_check(string str);
+
+#endif
